9ea9d3a96b12e058e9ad707d0e9718a999e6b20b
Atul Mahajan <luta_atul@hotmail.com>
2020-03-09 20:57:06 +1100

    Added fail function for ajax.

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,40 +39,44 @@ $(document).ready(function() {
     $.ajax({
       url: queryURL,
       method: "GET"
-    }).then(function(response) {
-      // .btn.preventDefault();
-      // $(".btn").on("click", displayResult);
-      displayResult();
-      function displayResult() {
-        var iconcode = response.list[0].weather[0].icon;
-        var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    })
+      .then(function(response) {
+        // .btn.preventDefault();
+        // $(".btn").on("click", displayResult);
+        displayResult();
+        function displayResult() {
+          var iconcode = response.list[0].weather[0].icon;
+          var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
-        localStorage.setItem("searchedCity", response.city.name);
-        $("#results").append("City: " + response.city.name + "<br>");
-        $("#results").append("Date: " + response.list[0].dt_txt + "<br>");
-        $("#results").append(
-          "Weather Description: " +
-            response.list[0].weather[0].description +
-            "<br>"
-        );
-        $("#results").append(
-          "Icon: " + response.list[0].weather[0].icon + "<br>"
-        );
-        $("#results").append(
-          "Temprature: " + response.list[0].main.temp + "<br>"
-        );
-        $("#results").append(
-          "Humidity: " + response.list[0].main.humidity + "<br>"
-        );
-        $("#results").append(
-          "Wind Speed: " + response.list[0].wind.speed + "<br>"
-        );
+          localStorage.setItem("searchedCity", response.city.name);
+          $("#results").append("City: " + response.city.name + "<br>");
+          $("#results").append("Date: " + response.list[0].dt_txt + "<br>");
+          $("#results").append(
+            "Weather Description: " +
+              response.list[0].weather[0].description +
+              "<br>"
+          );
+          $("#results").append(
+            "Icon: " + response.list[0].weather[0].icon + "<br>"
+          );
+          $("#results").append(
+            "Temprature: " + response.list[0].main.temp + "<br>"
+          );
+          $("#results").append(
+            "Humidity: " + response.list[0].main.humidity + "<br>"
+          );
+          $("#results").append(
+            "Wind Speed: " + response.list[0].wind.speed + "<br>"
+          );
 
-        var imageIcon = $("<img>");
-        $(imageIcon).attr("src", iconurl);
+          var imageIcon = $("<img>");
+          $(imageIcon).attr("src", iconurl);
 
-        $("#results").append(imageIcon);
-      }
-    });
+          $("#results").append(imageIcon);
+        }
+      })
+      .fail(function(response) {
+        $("#results").append("<h1>No data found.<br>");
+      });
   }
 });
